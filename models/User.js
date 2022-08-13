7f41b00121ff72c35c0f4514f6198b26fc651845
ReenaH <Reena_Hunjan@yahoo.com>
2022-08-12 20:46:28 -0400

    password

--- models/User.js
+++ models/User.js
@@ -2,7 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
  
 User.init(
