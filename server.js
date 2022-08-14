80b46f3906f5afea7c4ed3e4f5e242bd5b5b63d2
Gurneet <gsingh2@atb.com>
2022-08-14 15:09:57 -0400

    handlebars hellow world

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
