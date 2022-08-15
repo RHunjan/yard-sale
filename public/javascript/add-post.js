a33b3fa826b958bac7cd77e686878072f7cf1a58
Alex <Alex@Samanthas-MacBook-Pro.local>
2022-08-15 16:55:42 -0400

    updated addpost

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,13 +2,13 @@ async function newFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value;
-    const post_url = document.querySelector('input[name="post-url"]').value;
+   
   
     const response = await fetch(`/api/items`, {
       method: 'POST',
       body: JSON.stringify({
         title,
-        post_url
+     
       }),
       headers: {
         'Content-Type': 'application/json'
