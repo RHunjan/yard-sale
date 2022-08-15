61a3278c8b4d844fbace534f7ea15885fbbf0c3c
ReenaH <Reena_Hunjan@yahoo.com>
2022-08-14 22:42:18 -0400

    signup and logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment, Category } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
   Post.findAll({
     attributes: [
       'id',
@@ -35,6 +36,29 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+//login page
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+//logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+
 });
 
 module.exports = router;
