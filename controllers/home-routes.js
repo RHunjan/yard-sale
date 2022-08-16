e88338a9c86dabdf05c26b58d2a9b905d67927a1
Gurneet <gsingh2@atb.com>
2022-08-16 00:50:35 -0400

    connected landing page to login to homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -40,6 +40,42 @@ router.get('/', (req, res) => {
   res.render('landing');
 });
 
+router.get('/homepage', (req, res) => {
+  console.log(req.session);
+Post.findAll({
+  attributes: [
+    'id',
+    'title',
+    'post_description',
+    'post_price',
+    'created_at',
+    ],
+  include: [
+    {
+      model: Comment,
+      attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+      include: {
+        model: User,
+        attributes: ['username']
+      }
+    },
+    {
+      model: User,
+      attributes: ['username']
+    }
+  ]
+})
+  .then(dbPostData => {
+    // pass a single post object into the homepage template
+  const posts = dbPostData.map(post => post.get({ plain: true })); 
+  res.render('homepage', { posts });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
 //login page
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
